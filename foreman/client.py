9e79c001abc62040638a8dd1cb0a0b79868a7c73
Satoshi Konno <skonno@cybergarage.org>
2018-03-02 13:30:21 +0900

    Updated Console and Client to execute FQL.

--- foreman/client.py
+++ foreman/client.py
@@ -8,9 +8,51 @@
 #
 ##################################################################
 
+import urllib
+import requests
+
+from .constants import FOREMAN_DEFAULT_SERVER_HOST, FOREMAN_DEFAULT_HTTP_PORT, FOREMAN_HTTP_SERVER_FQL_PATH, FOREMAN_HTTP_SERVER_FQL_QUERY
+from .constants import FOREMAN_CONFIG_CATEGORY_KEY, FOREMAN_CONFIG_PRODUCT_KEY, FOREMAN_CONFIG_VERSION_KEY
+from .exceptions import ConnectionError
+
 class Client:
     def __init__(self):
-        pass
+        self.host = FOREMAN_DEFAULT_SERVER_HOST
+        self.port = FOREMAN_DEFAULT_HTTP_PORT
 
-    def query(self, q):
-        return "hello"
+    def query(self, query):
+        try:
+            url = self.__create_query_url(query)
+            res = requests.get(url)
+            return res
+        except requests.exceptions.ConnectionError:
+            raise ConnectionError(self.host, self.port)
+        except:
+            raise
+
+    def query_text(self, query):
+        res = self.query(query)
+        if res.status_code != 200:
+            return 'INVALID REQUEST : %s (%d)' % (query, res.status_code)
+        return res.text
+
+    def query_json(self, query):
+        res = self.query(query)
+        if res.status_code != 200:
+            return None
+        return res.json()
+
+    def query_version(self):
+        query = 'EXPORT CONFIG'
+        json = self.query_json(query)
+        product = json[FOREMAN_CONFIG_CATEGORY_KEY][FOREMAN_CONFIG_PRODUCT_KEY]
+        ver = json[FOREMAN_CONFIG_CATEGORY_KEY][FOREMAN_CONFIG_VERSION_KEY]
+        return '%s v%s (%s:%d)' % (product.capitalize(), ver, self.host, self.port)
+
+    def __create_query_url(self, query):
+        url = 'http://%s:%d%s?%s' % (
+            self.host, 
+            self.port, 
+            FOREMAN_HTTP_SERVER_FQL_PATH,
+            urllib.urlencode({FOREMAN_HTTP_SERVER_FQL_QUERY : query}))
+        return url
