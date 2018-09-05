3bf42a4365f1a658fd1f9d2563d6e4a594e4b9ab
Satoshi Konno <skonno@yahoo-corp.jp>
2018-09-05 19:33:58 +0900

    Update to catch any exceptions and output the messages

--- foreman/fql.py
+++ foreman/fql.py
@@ -11,6 +11,7 @@
 if __name__ == '__main__':
     import sys
     import select
+
     if __package__ is None:
         from os import path
         sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
@@ -18,11 +19,12 @@ if __name__ == '__main__':
     else:
         from .console import Console
 
-    c = Console()
-    if select.select([sys.stdin,],[],[],0.0)[0]:
-        c.tty = False
-
-    if c.tty:
-        c.output_version()
-
-    c.loop()
+    try:
+        c = Console()
+        if select.select([sys.stdin,],[],[],0.0)[0]:
+             c.tty = False
+        if c.tty:
+            c.output_version()
+        c.loop()
+    except Exception as e:
+        print("ERROR: " + e.message)
