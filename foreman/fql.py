fd5fa0d5dcdd173549547d5ee26aafa850c2bd2a
Shuhei Sekita <ssekita@yahoo-corp.jp>
2019-01-10 15:17:58 +0900

    Package

--- foreman/fql.py
+++ foreman/fql.py
@@ -8,30 +8,37 @@
 #
 ##################################################################
 
+from __future__ import print_function
+import sys
+import select
+from argparse import ArgumentParser
+from foreman.console import Console
+
+
+def main():
+    console = Console()
+
+    parser = ArgumentParser(description="Foreman client")
+    parser.add_argument(
+        "-H", "--host", default="localhost", help="Foreman hostname")
+    parser.add_argument(
+        "-P", "--port", type=int, default=8188, help="Foreman RPC port")
+    parser.add_argument(
+        "-E", "--execute", help="FQL statement")
+    args = parser.parse_args()
+
+    console.set_host(args.host)
+    console.set_port(args.port)
+    if args.execute is not None:
+        console.execute(args.execute)
+        quit()
+
+    if select.select([sys.stdin, ], [], [], 0.0)[0]:
+        console.tty = False
+    if console.tty:
+        console.output_version()
+    console.loop()
+
+
 if __name__ == '__main__':
-    import sys
-    import select
-
-    if __package__ is None:
-        from os import path
-        sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
-        from foreman import Console
-    else:
-        from .console import Console
-
-    c = Console()
-    
-    args = sys.argv
-    if 2 <= len(args):
-        c.set_host(args[1])
-    if 3 <= len(args):
-        c.set_port(int(args[2]))
-
-    try:
-        if select.select([sys.stdin,],[],[],0.0)[0]:
-             c.tty = False
-        if c.tty:
-            c.output_version()
-        c.loop()
-    except Exception as e:
-        print("ERROR: " + e.message)
+    main()
